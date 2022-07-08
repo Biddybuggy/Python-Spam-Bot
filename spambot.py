0dc710e5a2c7da64ee554dcef7b44eb2eb820147
Biddybuggy <100064835+Biddybuggy@users.noreply.github.com>
2022-07-08 10:16:51 +0700

    Update spambot.py

--- spambot.py
+++ spambot.py
@@ -1,14 +1,47 @@
-import time
 import pyautogui
-    
-needed_time = int(input("How many seconds do you think you would need to prepare? "))
+import time
 
-print(f"You got {needed_time} seconds to prepare...")
-time.sleep(needed_time)
-print("Spamming NOW")
+choice = int(input("1. Rickroll \n2. Jebait \n3. Shipping \nYour input: "))
+if choice == 1:
+    print("\n")
+    counter = 5
+    for u in range(5):
+        print(counter)
+        counter -= 1
+        time.sleep(1)
+    print("\nSpamming now!")
+    f = open("insert a path to a file with the rickroll lyrics", 'r')
+    for word in f:
+        pyautogui.typewrite(word)
+        pyautogui.press("enter")
+    print("I'm done spamming!")
 
-f = open('insert path here')
-for word in f:
-    pyautogui.typewrite(word)
-    pyautogui.press('enter')
+elif choice == 2:
+    print("\n")
+    counter = 5
+    for u in range(counter):
+        print(counter)
+        counter -= 1
+        time.sleep(1)
+    print("\nSpamming now!")
+    f = open("insert a path to a file with the jebaited lyrics", 'r')
+    for word in f:
+        pyautogui.typewrite(word)
+        pyautogui.press("enter")
+    print("I'm done spamming!")
 
+elif choice == 3:
+    print("\n")
+    counter = 5
+    for u in range(5):
+        print(counter)
+        counter -= 1
+        time.sleep(1)
+    print("\nSpamming now!")
+    for u in range(10):
+        pyautogui.typewrite("(insert name) likes (insert another name)")
+        pyautogui.press("enter")
+    print("I'm done spamming!")
+
+else:
+    print("\nInvalid input.")
